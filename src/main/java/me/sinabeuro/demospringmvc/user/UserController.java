ccb9b153ec755cc61fa6d3e909e2d63f2bc67f4f
sinabeuro53 <sinabeuro53@naver.com>
2020-04-12 21:32:57 +0900

    Initial commit

---
+++ src/main/java/me/sinabeuro/demospringmvc/user/UserController.java
@@ -0,0 +1,19 @@
+package me.sinabeuro.demospringmvc.user;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+public class UserController {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "hello";
+    }
+
+    @PostMapping("/users/create")
+    public User create(@RequestBody User user) {
+        return user;
+    }
+
+}
